f3db0c808f48ff98c8c2b2cbb08788e7ad229bc8
ajesh <ajesh.babu@ondace.com>
2022-02-01 17:19:23 +0530

    Added emission table

---
+++ src/pages/EmissionList/EmissionTable/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import CeroButton from '../../../components/CeroButton';
+
+import CeroTable from '../../../components/CeroTable';
+import useStyles from "./styles";
+
+const EmissionTable = (props) => {
+    const classes = useStyles();
+    const navigate = useNavigate();
+
+    const columns = [{
+        columnKey: 'fuel',
+        columnId: 'fuel',
+        columnHeader: 'Fuel',
+    }, {
+        columnKey: 'count',
+        columnId: 'count',
+        columnHeader: 'Active Data',
+    }, {
+        columnKey: 'co2',
+        columnId: 'co2',
+        columnHeader: 'CO2(tonnes)',
+    }, {
+        columnKey: 'ch4',
+        columnId: 'ch4',
+        columnHeader: 'CH4(tonnes)',
+    }, {
+        columnKey: 'n2o',
+        columnId: 'n2o',
+        columnHeader: 'N2O(tonnes)',
+    }, {
+        columnKey: 'co2e',
+        columnId: 'co2e',
+        columnHeader: 'CO2e(tonnes)',
+    }, {
+        columnKey: 'bioFuel',
+        columnId: 'bioFuel',
+        columnHeader: 'BioFuel CO2(tonnes)',
+    }, {
+        columnKey: 'ef',
+        columnId: 'ef',
+        columnHeader: 'EF (kgCO2e/unit)',
+    }, {
+        columnKey: 'action',
+        columnId: 'action',
+        columnHeader: '',
+    }]
+
+    const onSelectEmissionData = (emission) => {
+        navigate(`/emissions/${emission.id}`);
+    };
+
+    const getEmissionData = () => props.emissionData.map(item => ({
+        ...item,
+        action: <CeroButton className={classes.button} buttonText="Audited" />
+    }))
+
+
+    return (
+        <CeroTable
+            columns={columns}
+            data={getEmissionData()}
+            hasMore={false}
+            loading={false}
+            loadMore={props.onLoadMore}
+            onSelectRow={onSelectEmissionData}
+        />
+    )
+}
+
+export default EmissionTable;
---
+++ src/pages/EmissionList/index.jsx
@@ -0,0 +1,55 @@
+import { Container } from "@mui/material";
+import DasboardLayout from '../../layouts/DasboardLayout/'
+
+import EmissionTable from "./EmissionTable";
+import useStyles from "./styles";
+
+const savedPage = { // To be used from the api response
+    pageNumber: 1,
+};
+
+const DEFAULT_ITEMS_PER_PAGE = 20;
+
+const EmissionList = () => {
+    const classes = useStyles();
+    const emissonData = [{
+        fuel: 'CNC',
+        count: 10,
+        co2: '0.05',
+        ch4: '',
+        n2o: '',
+        co2e: '',
+        bioFuel: '',
+        ef: '',
+    }, {
+        fuel: 'CNC',
+        count: 10,
+        co2: '0.05',
+        ch4: '0.045',
+        n2o: '0.54',
+        co2e: '0.01',
+        bioFuel: '0.43',
+        ef: '1.006',
+    }];
+
+    const onLoadMore = (pageSize = DEFAULT_ITEMS_PER_PAGE, pageNumber) => {
+        const filter = {
+            emissionFilter: {},
+            page: {
+                pageSize,
+                pageNumber: pageNumber || (savedPage.pageNumber || 0) + 1,
+            }
+        };
+        // api call with filter
+    };
+
+    return (
+        <DasboardLayout>
+            <Container className={classes.container}>
+                <EmissionTable emissionData={emissonData} onLoadMore={onLoadMore} />
+            </Container>
+        </DasboardLayout>
+    );
+};
+
+export default EmissionList;
