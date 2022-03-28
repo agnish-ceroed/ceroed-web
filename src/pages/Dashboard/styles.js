cc76b86650f98bf439a463ae9b43f83b33c8ae40
ajesh <ajesh.babu@ondace.com>
2022-03-28 23:16:48 +0530

    Minor change

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -4,7 +4,7 @@ const useStyles = makeStyles((theme) => ({
     container: {
         padding: 0,
         width: '100%',
-        height: '100%'
+        // height: '100%'
     },
     gridContainer: {
         height: 'calc(100% - 64px)',
