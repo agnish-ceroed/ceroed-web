2c0eddb15f1f9d56abd09081ff48e52da362811d
Sharafudeen K <sharafudeen.k@lookout.com>
2022-04-24 15:12:26 +0530

    Add summary table

--- src/pages/ApprovalMonthlySummary/index.js
+++ src/pages/ApprovalMonthlySummary/index.js
@@ -7,8 +7,96 @@ import SearchBox from "../../components/SearchBox";
 import {sampleYear} from "../../constants";
 import { listFacilities } from "../../redux/actions";
 import Status from './Status'
+import MonthlySummaryTable from './MonthlySummaryTable'
 
 import useStyles from "./styles";
+
+const summaryData = [
+  {
+    date: 'Jan 2021',
+    approved_by: 'John Doe',
+    requested_by: 'Thomas Doe',
+    requested_on: '01/12/2021',
+    status: 'Pending'
+  },
+  {
+    date: 'Feb 2021',
+    approved_by: 'Marshal Doe',
+    requested_by: 'Thomas Doe',
+    requested_on: '05/08/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'March 2021',
+    approved_by: 'James Doe',
+    requested_by: 'John Doe',
+    requested_on: '02/09/2021',
+    status: 'Pending'
+  },
+  {
+    date: 'April 2021',
+    approved_by: 'Carol Doe',
+    requested_by: 'Susan Doe',
+    requested_on: '11/07/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'May 2021',
+    approved_by: 'Rose Geller',
+    requested_by: 'Monica Geller',
+    requested_on: '10/10/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'June 2021',
+    approved_by: 'Chandler Bing',
+    requested_by: 'Joy Thomas',
+    requested_on: '05/06/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'July 2021',
+    approved_by: 'Will Smith',
+    requested_by: 'Penny Smith',
+    requested_on: '06/04/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'August 2021',
+    approved_by: 'Marshal Thomas',
+    requested_by: 'Lilly Mary',
+    requested_on: '28/12/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'September 2021',
+    approved_by: 'Lucifer Morningstar',
+    requested_by: 'Chloe Decker',
+    requested_on: '01/12/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'October 2021',
+    approved_by: 'Jon Snow',
+    requested_by: 'Caitlin Snow',
+    requested_on: '01/12/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'November 2021',
+    approved_by: 'Barry Allen',
+    requested_by: 'West Allen',
+    requested_on: '01/12/2021',
+    status: 'Approved'
+  },
+  {
+    date: 'December 2021',
+    approved_by: 'Wally West',
+    requested_by: 'Nora West',
+    requested_on: '01/12/2021',
+    status: 'Approved'
+  },
+]
 
 
 const ApprovalMonthlySummary = () => {
@@ -68,6 +156,7 @@ const ApprovalMonthlySummary = () => {
           </Grid>
         </Grid>
         <Status/>
+        <MonthlySummaryTable summaryData={summaryData}/>
       </Container>
     </DashboardLayout>
   );
--- src/pages/ApprovalMonthlySummary/styles.js
+++ src/pages/ApprovalMonthlySummary/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
     container: {
         padding: theme.spacing(2),
+        height: '100%'
     }
 }));
 
