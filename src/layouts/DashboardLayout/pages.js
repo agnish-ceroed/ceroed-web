d1f52adca27783f22efec90e234c579e55d4d69d
ajesh <ajesh.babu@ondace.com>
2022-05-01 18:39:23 +0530

    file added

---
+++ src/layouts/DashboardLayout/pages.js
@@ -0,0 +1,94 @@
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ViewColumnOutlinedIcon from '@mui/icons-material/ViewColumnOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import CategoryIcon from '@mui/icons-material/Category';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+
+
+export const rolesEnum = {
+    ADMIN: 'admin',
+    AUDITOR: 'auditor'
+}
+
+export const sideMenuItems = [
+    {
+        text: 'Dashboard',
+        icon: <DashboardOutlinedIcon/>,
+        path: '/dashboard',
+        roles: [rolesEnum.ADMIN, rolesEnum.AUDITOR]
+    },
+    {
+        text: 'Emissions',
+        icon: <MoreHorizIcon/>,
+        path: '/emissions',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Simulations',
+        icon: <MailOutlineIcon/>,
+        path: '/simulations',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Goals',
+        icon: <PersonOutlineIcon/>,
+        path: '/goals',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Reports',
+        icon: <ChatBubbleOutlineOutlinedIcon/>,
+        path: '/reports',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Benchmarking',
+        icon: <ViewColumnOutlinedIcon/>,
+        path: '/benchmarking',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Facilities',
+        icon: <CategoryIcon/>,
+        path: '/facilities',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'User Management',
+        icon: <PeopleAltIcon/>,
+        path: '/users',
+        roles: [rolesEnum.ADMIN]
+    },
+    {
+        text: 'Companies',
+        icon: <ApartmentIcon/>,
+        path: '/companies',
+        roles: [rolesEnum.AUDITOR]
+    },
+]
+
+export const settingsOption = [
+    {
+        text: 'My Profile',
+        icon: <ManageAccountsOutlinedIcon/>,
+        path: '/profile',
+        roles: [rolesEnum.AUDITOR, rolesEnum.ADMIN]
+    },
+    {
+        text: 'Help',
+        icon: <ViewColumnOutlinedIcon/>,
+        path: '/help',
+        roles: [rolesEnum.AUDITOR, rolesEnum.ADMIN]
+    },
+    {
+        text: 'Settings',
+        icon: <ManageAccountsOutlinedIcon/>,
+        path: '/settings',
+        roles: [rolesEnum.AUDITOR, rolesEnum.ADMIN]
+    },
+]
