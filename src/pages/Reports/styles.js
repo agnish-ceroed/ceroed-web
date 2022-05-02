9b6fc3583ecf3477250c001e6d233ffcf74f5c58
sharafuvyt <40364489+sharafuvyt@users.noreply.github.com>
2022-05-02 23:04:08 +0530

    Add reports page (#83)
    Co-authored-by: Sharafudeen K <sharafudeen.k@lookout.com>

--- src/pages/Reports/styles.js
+++ src/pages/Reports/styles.js
@@ -1,9 +1,29 @@
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        padding: theme.spacing(2),
-    }
+  container: {
+    padding: theme.spacing(2),
+    height: "100%",
+  },
+  tableContainer: {
+    padding: 0,
+    width: "100%",
+    maxHeight: "calc(100% - 40px)",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    background: theme.palette.background.white,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  buttonPrimary: {
+    width: 160,
+    height: 42,
+    fontSize: 13,
+    backgroundColor: theme.palette.text.accent,
+  },
 }));
 
 export default useStyles;
