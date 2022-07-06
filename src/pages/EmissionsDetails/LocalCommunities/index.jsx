a9e3ea27a9b5979959825eecb90e41021bee81d1
sharafuvyt <40364489+sharafuvyt@users.noreply.github.com>
2022-07-06 20:09:58 +0530

    New Emission details page (#140)
    * Add emission details page
    
    * Emission details added
    
    Co-authored-by: Sharafudeen K <sharafudeen.k@lookout.com>

---
+++ src/pages/EmissionsDetails/LocalCommunities/index.jsx
@@ -0,0 +1,207 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import clsx from "clsx";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Container,
+  Grid,
+  Typography,
+  Box,
+  Tabs,
+  Tab,
+  IconButton,
+} from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { useSnackbar } from "notistack";
+import dayjs from "dayjs";
+
+import { STATUS } from "../../../redux/constants";
+import {
+  resetAddCombustionStatus,
+  deleteEmissions,
+} from "../../../redux/actions";
+import { getMonth } from "../../../services/utilityService";
+import CeroButton from "../../../components/CeroButton";
+import ListComments from "../ListComment";
+import ListAuditTrails from "../ListAuditTrails";
+import ListEmissionFiles from "../ListEmissionFiles";
+import ListTicketDrawer from "../../common/ListTicketsDrawer";
+import CeroConfirmDrawer from "../../../components/CeroConfirmDrawer";
+import useStyles from "./styles";
+
+const LocalCommunities = (props) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+  const {
+    emissionId,
+    emissionData,
+    onCancel,
+    isDeleteEnable,
+    setIsDrawerOpen,
+  } = props;
+
+  const [value, setValue] = useState(0);
+  const [showTickets, setShowTickets] = useState(false);
+  const [displayWarning, setDisplayWarning] = useState(false);
+
+  const deleteEmissionData = useSelector(
+    (state) => state.emission.deleteEmissions
+  );
+
+  useEffect(() => {
+    if (deleteEmissionData.status === STATUS.SUCCESS) {
+      enqueueSnackbar(
+        "Local Communities deleted successfully",
+        {
+          variant: "success",
+        }
+      );
+      dispatch(resetAddCombustionStatus());
+      onCancel();
+    } else if (deleteEmissionData.status === STATUS.ERROR) {
+      enqueueSnackbar("Something went wrong", { variant: "error" });
+      dispatch(resetAddCombustionStatus());
+    }
+  }, [deleteEmissionData, enqueueSnackbar, onCancel, dispatch]);
+
+  const onUpdatePurchasedElectricity = () => {
+    navigate(
+      `/emissions/edit/local_communities/${emissionId}`
+    );
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const onConfirmDelete = () => {
+    const requestData = {
+      id: emissionId,
+    };
+    dispatch(deleteEmissions(requestData));
+  };
+
+  const onclickShowTickets = () => {
+    setShowTickets(true);
+  };
+
+  const onCloseTickets = () => {
+    setShowTickets(false);
+  };
+
+  return (
+    <Container className={classes.container}>
+      <Box className={classes.innerContainer}>
+        <Box className={classes.header}>
+          <Typography variant="h6" component="div">
+            Local Communities
+          </Typography>
+          <Box>
+            <CeroButton
+              variant="outlined"
+              buttonText="Raise a ticket"
+              className={classes.buttonSecondary}
+              onClick={() => setIsDrawerOpen(true)}
+            />
+            <IconButton onClick={onclickShowTickets}>
+              <AssignmentIcon />
+            </IconButton>
+          </Box>
+        </Box>
+        <Box className={classes.topContainer}>
+          <Grid
+            container
+            direction={"row"}
+            wrap="nowrap"
+            justifyContent={"space-between"}
+            spacing={12}
+          >
+            <Grid item container direction={"column"} xs={10}>
+              <Typography className={classes.previewItem}>
+                Details: {emissionData.details}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Facility: {emissionData.facility_name}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Department: {emissionData.department}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Corrective Action: {emissionData.corrective_action}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Month and year: {getMonth[emissionData.month]}{" "}
+                {emissionData.year}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Status: {emissionData.status}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Audited on: {emissionData.audited_by}
+              </Typography>
+              <Typography className={classes.previewItem}>
+                Audited by:{" "}
+                {emissionData.audited_on
+                  ? dayjs(emissionData.audited_on).format("DD/MM/YYYY")
+                  : ""}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Box className={classes.buttonContainer}>
+        <CeroButton
+          buttonText="Back"
+          variant="outlined"
+          className={clsx(classes.button, classes.buttonSecondary)}
+          onClick={props.onCancel}
+        />
+        {isDeleteEnable && (
+          <CeroButton
+            buttonText={<DeleteOutlineIcon />}
+            className={clsx(classes.button, classes.deleteButton)}
+            onClick={() => setDisplayWarning(true)}
+          />
+        )}
+        <CeroButton
+          buttonText="Update"
+          className={clsx(classes.button, classes.buttonPrimary)}
+          onClick={onUpdatePurchasedElectricity}
+        />
+      </Box>
+      <Box
+        className={classes.tabContainer}
+        sx={{ borderBottom: 1, borderColor: "divider" }}
+      >
+        <Tabs value={value} onChange={handleChange} aria-label="emission tabs">
+          <Tab label="Comments" id="comments" />
+          <Tab label="Audit History" id="audit-history" />
+          <Tab label="Attachment" id="files" />
+        </Tabs>
+      </Box>
+      {value === 0 && <ListComments emissionId={emissionId} />}
+      {value === 1 && <ListAuditTrails emissionId={emissionId} />}
+      {value === 2 && <ListEmissionFiles emissionId={emissionId} />}
+      {displayWarning && (
+        <CeroConfirmDrawer
+          isOpen={displayWarning}
+          onClose={() => setDisplayWarning(false)}
+          onConfirm={onConfirmDelete}
+        />
+      )}
+      {showTickets && (
+        <ListTicketDrawer
+          isOpen={showTickets}
+          scope="emission"
+          scopeId={emissionId}
+          onClose={onCloseTickets}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default LocalCommunities;
--- src/pages/EmissionsDetails/index.js
+++ src/pages/EmissionsDetails/index.js
@@ -14,6 +14,23 @@ import WaterConsumptionDetails from "./WaterConsumptionDetails";
 import WaterDischargeDetails from "./WaterDischargeDetails";
 import WasteCombustionDetails from "./WasteCombustionDetails";
 import TransportationDetails from "./Transportation";
+import DevelopmentAndTraining from "./DevelopmentAndTraining";
+import EmployeeHealthSafety from "./EmployeeHealthSafety";
+import WorkerSafetyTraining from "./WorkerSafetyTraining";
+import EmployeeTurnover from "./EmployeeTurnover";
+import AgeBasedStatistics from "./AgeBasedStatistics";
+import GenderBasedStatistics from "./GenderBasedStatistics";
+import DiscriminationIncident from "./DiscriminationIncident";
+import SupplierScreening from "./SupplierScreening";
+import OperationalHumanRightsTraining from "./OperationalHumanRightsTraining";
+import SocialEngagementHumanRightsTraining from "./SocialEngagementHumanRightsTraining";
+import LocalCommunities from "./LocalCommunities";
+import PoliticalContributions from "./PoliticalContributions";
+import AntiCorruptionDisclosure from "./AntiCorruptionDisclosure";
+import AntiCorruptionTraining from "./AntiCorruptionTraining";
+import AntiCompetitiveDisclosure from "./AntiCompetitiveDisclosure";
+import SubsidiesFinancialAssistance from "./SubsidiesFinancialAssistance";
+import Tax from "./Tax";
 import { rolesEnum } from "../../layouts/DashboardLayout/pages";
 import CreateTicketDrawer from "../common/CreateTicketDrawer";
 import useStyles from "./styles";
@@ -121,6 +138,159 @@ const EmissionsDetails = () => {
             )}
             {type === "waste" && (
               <WasteCombustionDetails
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "development_training" && (
+              <DevelopmentAndTraining
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "employee_health_safety_incident_record" && (
+              <EmployeeHealthSafety
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "worker_safety_training_procedures" && (
+              <WorkerSafetyTraining
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "employees_turnover" && (
+              <EmployeeTurnover
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "age_based_statistics" && (
+              <AgeBasedStatistics
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "gender_based_statistics" && (
+              <GenderBasedStatistics
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "discrimination_incident_record" && (
+              <DiscriminationIncident
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "supplier_screening" && (
+              <SupplierScreening
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "operational_human_rights_training" && (
+              <OperationalHumanRightsTraining
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "social_engagement_human_rights_training" && (
+              <SocialEngagementHumanRightsTraining
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "local_communities" && (
+              <LocalCommunities
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "political_contributions" && (
+              <PoliticalContributions
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "anti_corruption_disclosure" && (
+              <AntiCorruptionDisclosure
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "anti_corruption_training" && (
+              <AntiCorruptionTraining
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "anti_competitive_disclosure" && (
+              <AntiCompetitiveDisclosure
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "subsidies_financial_assistance" && (
+              <SubsidiesFinancialAssistance
+                onCancel={onCancel}
+                emissionId={id}
+                emissionData={emissionData}
+                isDeleteEnable={isDeleteEnable}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            )}
+            {type === "tax" && (
+              <Tax
                 onCancel={onCancel}
                 emissionId={id}
                 emissionData={emissionData}
